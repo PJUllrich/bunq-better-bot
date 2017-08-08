af01aebb6da8b91cdd4c854d7e3795146639d4be
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-08-08 14:51:16 +0200

    Remove not-yet-moved files

--- pinsparen/handler/basehandler.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-
-class BaseHandler(metaclass=abc.ABCMeta):
-
-    @abc.abstractmethod
-    def handle_event(self, msg_json):
-        """Handle incoming JSON data from a Callback Event
-        """
