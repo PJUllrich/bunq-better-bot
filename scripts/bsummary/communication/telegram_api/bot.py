df490f8b1b8045a675668ab4a619a2f00129f2ef
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-08-09 15:31:36 +0200

    Added an initial ConversationHandler to the TelegramBot for creating a budget

--- scripts/bsummary/communication/telegram_api/bot.py
+++ scripts/bsummary/communication/telegram_api/bot.py
@@ -1,6 +1,10 @@
 import logging
 
-from telegram.ext import CommandHandler, Updater
+from bunq.sdk.model.generated import MonetaryAccountBank
+from telegram import ChatAction, InlineKeyboardButton, InlineKeyboardMarkup, \
+    ReplyKeyboardMarkup
+from telegram.ext import CallbackQueryHandler, CommandHandler, \
+    ConversationHandler, Filters, MessageHandler, Updater
 
 from bsummary.communication.telegram_api import msg
 
@@ -8,6 +12,9 @@ logger = logging.getLogger(__name__)
 
 _WEBHOOK_URL = 'YOUR URL HERE (e.g. www.example.com)'
 _WEBHOOK_PATH = 'YOUR PATH HERE (e.g. telegram-bot'
+CREATE_NAME, CREATE_IBAN, CREATE_DURATION, CREATE_KEYWORDS = range(4)
+_INPUT_FINISH = 'END'
+
 _WEBHOOK_PORT = 8525
 
 _WEBHOOK_URLPATH = f'{_WEBHOOK_URL}/{_WEBHOOK_PATH}'
@@ -18,33 +25,41 @@ def owner_only(func):
         if self.chat_id is not None and self.chat_id != update.message.chat_id:
             bot.send_message(update.message.chat_id, msg.NO_PERMISSION)
         else:
-            func(self, bot, update)
+            return func(self, bot, update)
 
     return inner
 
 
-class Telegram:
-    def __init__(self, token):
+class TelegramBot:
+    def __init__(self, token, actions):
         self.chat_id = None
-        self.update_action = None
+        self.creation = {}
+        self.actions = actions
 
         self.updater = Updater(token=token)
         self.dispatcher = self.updater.dispatcher
 
         self.setup_handlers()
 
+        self.setup_webhook()
+
+    def setup_handlers(self):
+        handler_start = CommandHandler('start', self.start)
+        handler_update = CommandHandler('update', self.update)
+        handler_create = self._get_create_handler()
+
+        self.dispatcher.add_handler(handler_start)
+        self.dispatcher.add_handler(handler_update)
+        self.dispatcher.add_handler(handler_create)
+
+        self.dispatcher.add_error_handler(self.error)
+
+    def setup_webhook(self):
         self.updater.start_webhook(port=_WEBHOOK_PORT,
                                    url_path=_WEBHOOK_PATH,
                                    webhook_url=_WEBHOOK_URLPATH)
         self.updater.bot.set_webhook(url=_WEBHOOK_URLPATH)
         logger.info(f'Telegram bot is now listening on port {_WEBHOOK_PORT}.')
-
-    def setup_handlers(self):
-        handler_start = CommandHandler('start', self.start)
-        handler_update = CommandHandler('update', self.update)
-
-        self.dispatcher.add_handler(handler_start)
-        self.dispatcher.add_handler(handler_update)
 
     @owner_only
     def start(self, bot, update):
@@ -58,12 +73,9 @@ class Telegram:
     def update(self, bot, update):
         logger.info('/update command received')
 
-        if self.update_action is None:
-            logger.error('Update_action needs to be set before the update '
-                         'command can be used.')
-            return
+        bot.send_chat_action(chat_id=self.chat_id, action=ChatAction.TYPING)
 
-        budget_results = self.update_action()
+        budget_results = self.actions.calc_budgets()
         for res in budget_results:
             duration = self._get_duration(res.budget.days_covered)
 
@@ -80,3 +92,104 @@ class Telegram:
         else:
             duration = "yesterday"
         return duration
+
+    @owner_only
+    def create_start(self, bot, update):
+        logger.info('Create: A new budget creation was started.')
+
+        bot.send_message(update.message.chat_id, msg.CREATE_START)
+        bot.send_message(update.message.chat_id, msg.CREATE_NAME)
+
+        return CREATE_NAME
+
+    @owner_only
+    def create_name(self, bot, update):
+        logger.info('Create: Received name of budget.')
+
+        bot.send_chat_action(chat_id=self.chat_id, action=ChatAction.TYPING)
+
+        self.creation['name'] = update.message.text
+        accounts = self.actions.get_active_accounts()
+
+        reply_markup = self._get_iban_keyboard(accounts)
+
+        bot.send_message(update.message.chat_id, msg.CREATE_IBAN,
+                         reply_markup=reply_markup)
+
+        self.creation['IBANs'] = []
+
+        return CREATE_IBAN
+
+    @owner_only
+    def create_iban(self, bot, update):
+        data = update.callback_query.data
+
+        if data == _INPUT_FINISH:
+            return CREATE_DURATION
+
+        self.creation['IBANs'].append(data)
+        return CREATE_IBAN
+
+    @owner_only
+    def create_duration(self, bot, update):
+        logger.info('Create: Received duration of budget.')
+
+        reply_keyboard = [[1, 2, 3, 4, 'more']]
+        reply_markup = ReplyKeyboardMarkup(reply_keyboard,
+                                           one_time_keyboard=True)
+
+    @owner_only
+    def create_duration_more(self, bot, update):
+        pass
+
+    @owner_only
+    def create_keywords(self, bot, update):
+        pass
+
+    @owner_only
+    def create_cancel(self, bot, update):
+        bot.send_message(self.chat_id, msg.CREATE_CANCEL)
+        self.creation = {}
+
+        return ConversationHandler.END
+
+    def error(self, bot, update, error):
+        logging.warning('Update "%s" caused error "%s"' % (update, error))
+
+    def _get_create_handler(self):
+        return ConversationHandler(
+            entry_points=[CommandHandler('create', self.create_start)],
+
+            states={
+                CREATE_NAME: [MessageHandler(Filters.text, self.create_name)],
+                CREATE_IBAN: [CallbackQueryHandler(self.create_iban)],
+                CREATE_DURATION: [],
+                CREATE_KEYWORDS: []
+            },
+
+            fallbacks=[CommandHandler('cancel', self.create_cancel)]
+
+        )
+
+    def _get_iban_keyboard(self, accounts):
+        """
+
+        Parameters
+        ----------
+        accounts    : list[MonetaryAccountBank]
+
+        Returns
+        -------
+
+        """
+
+        keyboard = []
+        for acc in accounts:
+            desc = acc.description
+            IBAN = self.actions.get_iban(acc)
+            button = InlineKeyboardButton(f'{desc}', callback_data=IBAN)
+            keyboard.append([button])
+
+        button_end = InlineKeyboardButton('Done', callback_data=_INPUT_FINISH)
+        keyboard.append([button_end])
+        return InlineKeyboardMarkup(keyboard)
--- scripts/bsummary/communication/telegram_api/msg.py
+++ scripts/bsummary/communication/telegram_api/msg.py
@@ -1,4 +1,14 @@
 WELCOME = "Hello there! I'll give you regular updates for your " \
-               "bunq budgets!"
+          "bunq budgets!"
 NO_PERMISSION = "You don't have permission to contact this bot!"
-UPDATE = 'You spent {} Euro of your {} budget {}'
+ERROR = "Sorry, but an error occurred on my side."
+UPDATE = "You spent {} Euro of your {} budget {}"
+
+CREATE_START = "You want to create a new Budget! Great! " \
+               "Let's get going right away"
+CREATE_NAME = "Please enter a name for the new Budget"
+CREATE_IBAN = "Next, select the accounts, which the Budget should monitor"
+CREATE_DURATION = "How many days should the new Budget cover?"
+CREATE_KEYWORDS = "Enter any keywords which the new Budget should cover"
+CREATE_CANCEL = "You canceled the creation process. " \
+                "Will delete all information."
